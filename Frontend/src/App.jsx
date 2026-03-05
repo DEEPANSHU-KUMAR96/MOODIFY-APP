76721bbdcf2dc7c2c59eeb50d222d780734f0563
Deepanshu <deepanshukumar9650@gmail.com>
2026-03-05 15:23:35 +0530

    frontend changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import FaceExpression from './features/Expression/components/FaceExpression'
+import { RouterProvider } from "react-router"
+import { router } from "./app.routes"
+import "./features/shared/styles/global.scss"
+import { AuthProvider } from "./features/auth/auth.context"
 
-const App = () => {
+function App() {
   return (
 
-    <FaceExpression />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+
 
   )
 }
